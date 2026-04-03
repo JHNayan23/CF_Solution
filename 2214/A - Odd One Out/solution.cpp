3f52c03d952b2c963afb5c6dda870d13e456752a
JHNayan23 <119337726+JHNayan23@users.noreply.github.com>
2026-04-03 19:08:06 +0600

    Add Odd One Out [A] from Codeforces

---
+++ 2214/A - Odd One Out/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std;#define int long long int#define all(x) x.begin(), x.end() void solve() {    cout << "C2" << '\n';}int32_t main() {    ios_base::sync_with_stdio(0); cin.tie(0);    int t = 1; //cin >> t;    while (t--) solve();    return 0;}
