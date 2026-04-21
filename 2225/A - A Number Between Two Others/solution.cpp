6c5868bf510a6f08707e6d85ad01df8fe93d0948
JHNayan23 <119337726+JHNayan23@users.noreply.github.com>
2026-04-21 20:42:44 +0600

    Add A Number Between Two Others [A] from Codeforces

---
+++ 2225/A - A Number Between Two Others/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std;#define int long long int#define all(x) x.begin(), x.end() void solve() {    int x, y; cin >> x >> y;    int gap = abs(x - y);    if (gap > x) cout << "YES\n";    else cout << "NO\n";}int32_t main() {    ios_base::sync_with_stdio(0); cin.tie(0);    int t = 1; cin >> t;    while (t--) solve();    return 0;}
