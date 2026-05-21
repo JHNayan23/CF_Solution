df8bf150d401804e4b81468cabe050be34772887
JHNayan23 <119337726+JHNayan23@users.noreply.github.com>
2026-05-21 20:38:09 +0600

    Add Construct an Array [A] from Codeforces

---
+++ 2231/A - Construct an Array/solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define all(x) x.begin(), x.end()
+ 
+void solve() 
+{
+    int n; cin >> n;
+    for (int i = 0; i < n; i++) cout << 2 * i + 1 << ' '; cout << '
+';
+}
+int32_t main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t = 1; cin >> t;
+    while (t--) solve();
+    return 0;
+}
