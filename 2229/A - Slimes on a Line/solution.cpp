f2b9314107e3382faf326e24b3818d369e7db466
JHNayan23 <119337726+JHNayan23@users.noreply.github.com>
2026-05-23 20:46:55 +0600

    Add Slimes on a Line [A] from Codeforces

---
+++ 2229/A - Slimes on a Line/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define all(x) x.begin(), x.end()
+ 
+void solve() 
+{
+    int n; cin >> n;
+    vector<int> a(n);
+    for (auto &i : a) cin >> i;
+    cout << (*max_element(all(a)) - *min_element(all(a)) + 1) / 2 << '
+';
+}
+int32_t main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t = 1; cin >> t;
+    while (t--) solve();
+    return 0;
+}
