46ef266ee10ca0db0ab000edbe9a7602dee39ad1
JHNayan23 <119337726+JHNayan23@users.noreply.github.com>
2026-06-27 20:51:11 +0600

    Add Another Popcount Problem [A] from Codeforces

---
+++ 2240/A - Another Popcount Problem/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define all(x) x.begin(), x.end()
+ 
+void solve() 
+{
+    int n, k; cin >> n >> k;
+    int ans = 0;
+    for (int i = 1; i <= n; i <<= 1) {
+        int x = min(k, n / i);
+        ans += x;
+        n -= x * i;
+    }
+    cout << ans << '
+';
+}
+int32_t main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t = 1; cin >> t;
+    while (t--) solve();
+    return 0;
+}
