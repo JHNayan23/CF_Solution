7d953d5894b9611ec4019d9439bfbdb597cade81
JHNayan23 <119337726+JHNayan23@users.noreply.github.com>
2026-06-30 20:39:34 +0600

    Add Divide and Conquer [A] from Codeforces

---
+++ 2241/A - Divide and Conquer/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define all(x) x.begin(), x.end()
+ 
+void solve() 
+{
+    int x, y; cin >> x >> y;
+    cout << (x % y == 0 ? "YES
+" : "NO
+");
+}
+int32_t main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t = 1; cin >> t;
+    while (t--) solve();
+    return 0;
+}
